fa3f84ef7b72cd8b3b13379030acbd8025b1c8d9
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2019-03-20 16:44:21 +0530

    Cleanup

--- run_ner.py
+++ run_ner.py
@@ -66,20 +66,21 @@ def readfile(filename):
     return format :
     [ ['EU', 'B-ORG'], ['rejects', 'O'], ['German', 'B-MISC'], ['call', 'O'], ['to', 'O'], ['boycott', 'O'], ['British', 'B-MISC'], ['lamb', 'O'], ['.', 'O'] ]
     '''
-    f = open(filename)
-    data = []
-    sentence = []
-    label = []
-    for line in f:
-        if len(line) == 0 or line.startswith('-DOCSTART') or line[0] == "\n":
-            if len(sentence) > 0:
-                data.append((sentence, label))
-                sentence = []
-                label = []
-            continue
-        splits = line.split(' ')
-        sentence.append(splits[0])
-        label.append(splits[-1][:-1])
+    with open(filename) as f:
+        data = []
+        sentence = []
+        label = []
+        for line in f:
+            if len(line) == 0 or line.startswith('-DOCSTART') or line[0] == "\n":
+                if len(sentence) > 0:
+                    data.append((sentence, label))
+                    sentence = []
+                    label = []
+                continue
+            splits = line.split(' ')
+            sentence.append(splits[0])
+            label.append(splits[-1][:-1])
+            
 
     if len(sentence) > 0:
         data.append((sentence, label))
